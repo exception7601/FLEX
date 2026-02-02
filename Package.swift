7059e9b1e9ec231c10170fad2cc478602b7f5243
exception7601 <77030548+exception7601@users.noreply.github.com>
2026-02-02 17:39:32 -0300

    new Version 5.22.10.1770064732

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "FLEX",
-      url: "https://github.com/exception7601/FLEX/releases/download/5.22.10.1770064284/FLEX-e34d20ddc2c9d1f4.zip",
-      checksum: "e25a6465154fb2853ae05a6b66afcfd54fe7450c612c4a5d07ff9562e23f9e0f"
+      url: "https://github.com/exception7601/FLEX/releases/download/5.22.10.1770064732/FLEX-f0719eedc24db1de.zip",
+      checksum: "e7d18abe50d3f8227d482f8bae6dddbd33e000b0756ae1c671bf1628602a4c9b"
     )
   ]
 )
